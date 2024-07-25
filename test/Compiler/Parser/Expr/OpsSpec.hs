e028106f57750e3b4cbfc97ccf37f6eb55170de5
ki11errabbit <ki11errabbit@posteo.net>
2024-07-25 15:32:35 -0700

    fixed test setup

--- test/Compiler/Parser/Expr/OpsSpec.hs
+++ test/Compiler/Parser/Expr/OpsSpec.hs
@@ -1,14 +1,17 @@
-module OpsSpec (main, spec) where
+{-# OverloadedStrings #-}
+module Compiler.Parser.Expr.OpsSpec (main, spec) where
 
+import Text.Megaparsec
+import Compiler.Ast.Expr
+import Compiler.Ast.Shared
 import Compiler.Parser.Expr.Ops
 import Compiler.Parser.Expr.Shared
-import Compiler.Parser.Expr.Base (parseBaseExpr)
+import Compiler.Parser.Expr.Base
 import Test.Hspec
 import Test.Hspec.Megaparsec
 import Test.QuickCheck
-import Parser
-import Ast
 import Control.Monad.Combinators.Expr
+import Data.Text (pack)
 
 main :: IO ()
 main = hspec spec
@@ -17,21 +20,21 @@ spec :: Spec
 spec = do
   describe "parseOp" $ do
     it "parses a unary operator" $ do
-      parse (parseOp parseBaseExpr) "" "!12" `shouldParse` UnaryOp NotOp (Literal (IntLit "12"))
+      parse (parseOp parseBaseExpr) "" (pack "!12") `shouldParse` UnaryOp NotOp (Literal (IntLit "12"))
     it "parses a binary operator" $ do
-      parse (parseOp parseBaseExpr) "" "12 + 2" `shouldParse` BinaryOp Add (Literal (IntLit "12")) (Literal (IntLit "2"))
+      parse (parseOp parseBaseExpr) "" (pack "12 + 2") `shouldParse` BinaryOp Add (Literal (IntLit "12")) (Literal (IntLit "2"))
     it "parses a binary operator with a unary operator" $ do
-      parse (parseOp parseBaseExpr) "" "!12 + 2" `shouldParse` BinaryOp Add (UnaryOp NotOp (Literal (IntLit "12"))) (Literal (IntLit "2"))
+      parse (parseOp parseBaseExpr) "" (pack "!12 + 2") `shouldParse` BinaryOp Add (UnaryOp NotOp (Literal (IntLit "12"))) (Literal (IntLit "2"))
     it "parses a binary operator with a unary operator" $ do
-      parse (parseOp parseBaseExpr) "" "12 + !2" `shouldParse` BinaryOp Add (Literal (IntLit "12")) (UnaryOp NotOp (Literal (IntLit "2")))
+      parse (parseOp parseBaseExpr) "" (pack "12 + !2") `shouldParse` BinaryOp Add (Literal (IntLit "12")) (UnaryOp NotOp (Literal (IntLit "2")))
     it "parses a binary operator with a unary operator" $ do
-      parse (parseOp parseBaseExpr) "" "!12 + !2" `shouldParse` BinaryOp Add (UnaryOp NotOp (Literal (IntLit "12"))) (UnaryOp NotOp (Literal (IntLit "2")))
+      parse (parseOp parseBaseExpr) "" (pack "!12 + !2") `shouldParse` BinaryOp Add (UnaryOp NotOp (Literal (IntLit "12"))) (UnaryOp NotOp (Literal (IntLit "2")))
     it "parses a binary operator with a unary operator" $ do
-      parse (parseOp parseBaseExpr) "" "!12 + !2" `shouldParse` BinaryOp Add (UnaryOp NotOp (Literal (IntLit "12"))) (UnaryOp NotOp (Literal (IntLit "2")))
+      parse (parseOp parseBaseExpr) "" (pack "!12 + !2") `shouldParse` BinaryOp Add (UnaryOp NotOp (Literal (IntLit "12"))) (UnaryOp NotOp (Literal (IntLit "2")))
     it "parses a binary operator with a unary operator" $ do
-      parse (parseOp parseBaseExpr) "" "!12 + !2" `shouldParse` BinaryOp Add (UnaryOp NotOp (Literal (IntLit "12"))) (UnaryOp NotOp (Literal (IntLit "2")))
+      parse (parseOp parseBaseExpr) "" (pack "!12 + !2") `shouldParse` BinaryOp Add (UnaryOp NotOp (Literal (IntLit "12"))) (UnaryOp NotOp (Literal (IntLit "2")))
     it "parses a binary operator with a unary operator" $ do
-      parse (parseOp parseBaseExpr) "" "!12 + !2" `shouldParse` BinaryOp Add (UnaryOp NotOp (Literal (IntLit "12"))) (UnaryOp NotOp (Literal (IntLit "2")))
+      parse (parseOp parseBaseExpr) "" (pack "!12 + !2") `shouldParse` BinaryOp Add (UnaryOp NotOp (Literal (IntLit "12"))) (UnaryOp NotOp (Literal (IntLit "2")))
     it "parses a binary operator with a unary operator" $ do
-      parse (parseOp parseBaseExpr) "" "!12 + !2" `shouldParse` BinaryOp Add (UnaryOp NotOp (Literal (IntLit "12"))) (UnaryOp NotOp (Literal (IntLit "2")))
+      parse (parseOp parseBaseExpr) "" (pack "!12 + !2") `shouldParse` BinaryOp Add (UnaryOp NotOp (Literal (IntLit "12"))) (UnaryOp NotOp (Literal (IntLit "2")))
 
