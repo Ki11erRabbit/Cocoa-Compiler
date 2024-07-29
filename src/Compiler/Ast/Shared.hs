b0af5c65de94c3b60372bc92aade876eb6afd640
ki11errabbit <ki11errabbit@posteo.net>
2024-07-29 16:31:11 -0700

    added way to reference other object members

--- src/Compiler/Ast/Shared.hs
+++ src/Compiler/Ast/Shared.hs
@@ -1,18 +1,37 @@
 module Compiler.Ast.Shared where
 
 import Data.HashMap.Strict as H
+import Data.Hashable
 
 data Path = Path [String]
   deriving (Show, Eq)
 
+instance Hashable Path where
+  hashWithSalt salt (Path path) = hashWithSalt salt path
+
+
 data Visibility = PublicVis | ProtectedVis | PrivateVis
-  deriving (Show, Eq)
+  deriving (Show, Eq, Enum)
+
+instance Hashable Visibility where
+  hashWithSalt = hashUsing fromEnum
 
 data Type = Primitive PrimitiveType | Array Type | ClassType Path | Arguments Type [Type] | GenericType Type [TypeParam] | MethodType [TypeParam] [Type] Type
   deriving (Show, Eq)
 
+instance Hashable Type where
+  hashWithSalt salt (Primitive p) = hashWithSalt salt p
+  hashWithSalt salt (Array t) = hashWithSalt salt t
+  hashWithSalt salt (ClassType p) = hashWithSalt salt p
+  hashWithSalt salt (Arguments t ts) = hashWithSalt salt (t, ts)
+  hashWithSalt salt (GenericType t ts) = hashWithSalt salt (t, ts)
+  hashWithSalt salt (MethodType ts ts' t) = hashWithSalt salt (ts, ts', t)
+
 data PrimitiveType = UnitPrimType | U8PrimType | U16PrimType | U32PrimType | U64PrimType | I8PrimType | I16PrimType | I32PrimType | I64PrimType | F32PrimType | F64PrimType | BoolPrimType | CharPrimType
-  deriving (Show, Eq)
+  deriving (Show, Eq, Enum)
+
+instance Hashable PrimitiveType where
+  hashWithSalt = hashUsing fromEnum
 
 --data ClosureBody = ClosureBody [Statement]
 --  deriving (Show)
@@ -20,6 +39,10 @@ data PrimitiveType = UnitPrimType | U8PrimType | U16PrimType | U32PrimType | U64
 
 data TypeParam = Generic String [Type] | Concrete Type
   deriving (Show, Eq)
+
+instance Hashable TypeParam where
+  hashWithSalt salt (Generic s ts) = hashWithSalt salt (s, ts)
+  hashWithSalt salt (Concrete t) = hashWithSalt salt t
 
 class TypeUtils a where
   getTypes :: [String] -> H.HashMap [String] [Type] -> [[String]] -> Maybe Type -> a -> Either String [Type]
--- src/Compiler/TypeChecker.hs
+++ src/Compiler/TypeChecker.hs
@@ -11,6 +11,11 @@ import Data.HashSet as S
 import Data.Vector as V
 import Debug.Trace (trace)
 
+data OMPValue = SuperRef Type | Direct (H.HashMap [String] [Type])
+
+type ObjectMemberTypes = H.HashMap Type [OMPValue] 
+type TypeMap = H.HashMap [String] [Type]
+
 emptyMapWithObject :: H.HashMap [String] [Type]
 emptyMapWithObject = H.insert ["Object"] [ClassType (Path ["Object"])] H.empty
 
@@ -19,37 +24,39 @@ emptyMapWithoutObject = H.empty
 
 
 topLevelTypeCheck :: V.Vector File -> [Int] -> Either String ()
-topLevelTypeCheck files order = checkFiles files order emptyMapWithObject
+topLevelTypeCheck files order = checkFiles files order emptyMapWithObject H.empty
 
 
-checkFiles :: V.Vector File -> [Int] -> H.HashMap [String] [Type] -> Either String ()
-checkFiles files (next:rest) types = do
+checkFiles :: V.Vector File -> [Int] -> TypeMap -> ObjectMemberTypes -> Either String ()
+checkFiles files (next:rest) types omt = do
   _ <- trace (show next Prelude.++ show rest) return $ ()
   let file = files V.! next
-  newTypes <- checkFile file types
-  checkFiles files rest newTypes
-checkFiles files [] types = Right ()
+  (newTypes, newOMT) <- checkFile file types omt
+  checkFiles files rest newTypes newOMT
+checkFiles files [] types omt = Right ()
 
-checkFile :: File -> H.HashMap [String] [Type] -> Either String (H.HashMap [String] [Type])
-checkFile File{packageDec=(PackageDec (Path package)), primaryClass=primaryClass, imports=imports} types = do
+checkFile :: File -> TypeMap -> ObjectMemberTypes -> Either String (TypeMap, ObjectMemberTypes)
+checkFile File{packageDec=(PackageDec (Path package)), primaryClass=primaryClass, imports=imports} types omt = do
   let imports' = Prelude.map (\(ImportDec (Path path)) -> path) imports
   typesToAdd <- (getTypes package types imports' Nothing primaryClass)
   let newTypes = H.insert (package Prelude.++ [className primaryClass]) typesToAdd types
-  checkClass primaryClass typesToAdd newTypes package imports'
+  checkClass primaryClass typesToAdd newTypes omt package imports'
   
-checkClass :: Class -> [Type] -> H.HashMap [String] [Type] -> [String] -> [[String]] -> Either String (H.HashMap [String] [Type])
-checkClass Class{className=className, members=members} classTypes types package imports = do
-  let fieldTypes = Prelude.foldl (\memberTypes field -> case field of
-                                      FieldMember (Field _ fieldName fieldType) -> H.insert ("this":fieldName:[]) [fieldType] memberTypes
+checkClass :: Class -> [Type] -> TypeMap -> ObjectMemberTypes -> [String] -> [[String]] -> Either String (TypeMap, ObjectMemberTypes)
+checkClass Class{className=className, members=members} classTypes types omt package imports = do
+  let classDirectOMTValue = Prelude.foldl (\memberTypes field -> case field of
+                                      FieldMember (Field _ fieldName fieldType) -> H.insert (fieldName:[]) [fieldType] memberTypes
                                       _ -> memberTypes) H.empty members
-  let fieldTypes' = H.insert ["this"] [Prelude.head classTypes] $ H.insert ["super"] (Prelude.tail classTypes) fieldTypes
-  let localTypes = [fieldTypes']
+  let fieldTypes = H.insert ["this"] [Prelude.head classTypes] $ H.insert ["super"] (Prelude.tail classTypes) H.empty
+  let localTypes = [fieldTypes]
   let methodTypes = Prelude.foldl (\acc member -> case member of
                                       MethodMember method -> ((methodName method), (getTypes package types imports (Just (Prelude.head classTypes)) method)):acc
                                       _ -> acc) [] members
   let (methodNames, methodTypes') = Prelude.unzip methodTypes
-  methodTypes'' <- Prelude.sequence methodTypes' {- TODO: check subclasses and deal with overloading -} 
-  let newTypes = trace "newTypes" $ Prelude.foldl (\acc (name, types) -> H.insert (package Prelude.++ className:[name]) types acc) types $ Prelude.zip methodNames methodTypes''
+  methodTypes'' <- Prelude.sequence methodTypes' {- TODO: check subclasses and deal with overloading -}
+  let classDirectOMTValue' = Prelude.foldl (\acc (name, types) -> H.insert [name] types acc) classDirectOMTValue $ Prelude.zip methodNames methodTypes''
+  let omtValue = (Direct classDirectOMTValue):(Prelude.map SuperRef $ Prelude.tail classTypes)
+  let newOMT = H.insert (Prelude.head classTypes) omtValue omt
   let methods = Prelude.filter (\member -> case member of
                                    MethodMember _ -> True
                                    _ -> False) members
@@ -58,82 +65,82 @@ checkClass Class{className=className, members=members} classTypes types package 
                                   (Right _, Right _) -> Right ()
                                   (Right _, Left msg) -> Left msg
                                   (Left msg, Left msg2) -> Left (msg Prelude.++ "\n" Prelude.++ msg2)) (Right ())
-                $ Prelude.map (\(method, ty) -> checkMethod method ty newTypes localTypes)
+                $ Prelude.map (\(method, ty) -> checkMethod method ty types localTypes newOMT)
                 $ Prelude.zip methods $ Prelude.concat methodTypes'')
-  return $ newTypes
+  return $ (types, newOMT)
 
 
-checkMethod :: Member -> Type -> (H.HashMap [String] [Type]) -> [(H.HashMap [String] [Type])] -> Either String ()
-checkMethod (MethodMember (Method{params=params, body=Prototype})) _ types localTypes = Right ()
-checkMethod (MethodMember (Method{params=params, body=(Native _)})) _ types localTypes = Right ()
-checkMethod (MethodMember (Method{params=params, body=(Redirect _)})) _ types localTypes = Right ()
-checkMethod (MethodMember (Method{params=params, body=(MethodBody stmts)})) methodType types localTypes = do
+checkMethod :: Member -> Type -> TypeMap -> [TypeMap] -> ObjectMemberTypes -> Either String ()
+checkMethod (MethodMember (Method{params=params, body=Prototype})) _ types localTypes _ = Right ()
+checkMethod (MethodMember (Method{params=params, body=(Native _)})) _ types localTypes _ = Right ()
+checkMethod (MethodMember (Method{params=params, body=(Redirect _)})) _ types localTypes _ = Right ()
+checkMethod (MethodMember (Method{params=params, body=(MethodBody stmts)})) methodType types localTypes omt = do
                 let localTypes' = (loadParams params):localTypes
-                checkStatements stmts methodType types localTypes
+                checkStatements stmts methodType types localTypes omt
   where
     loadParams (Param{..}:rest) = H.insert [paramName] [paramType] (loadParams rest)
     loadParams [] = H.empty
-checkMethod _ _ _ _ = Left "Method was not a method"
+checkMethod _ _ _ _ _ = Left "Method was not a method"
   
 
-checkStatements :: [Statement] -> Type -> (H.HashMap [String] [Type]) -> [(H.HashMap [String] [Type])] -> Either String ()
-checkStatements (stmt:rest) methodType types localTypes = do
-  localTypes' <- checkStatement stmt methodType types localTypes
-  checkStatements rest methodType types localTypes'
-checkStatements [] _ _ _ = Right ()
+checkStatements :: [Statement] -> Type -> TypeMap -> [TypeMap] -> ObjectMemberTypes -> Either String ()
+checkStatements (stmt:rest) methodType types localTypes omt = do
+  localTypes' <- checkStatement stmt methodType types localTypes omt
+  checkStatements rest methodType types localTypes' omt
+checkStatements [] _ _ _ _ = Right ()
 
 
-checkStatement :: Statement -> Type -> (H.HashMap [String] [Type]) -> [(H.HashMap [String] [Type])] -> Either String [(H.HashMap [String] [Type])]
-checkStatement (LetStmt (LetStatement (LetVar varName) (Just ty) expr)) methodType types localTypes = do
-  exprTypes <- inferExpr expr types localTypes
+checkStatement :: Statement -> Type -> TypeMap -> [TypeMap] -> ObjectMemberTypes -> Either String [TypeMap]
+checkStatement (LetStmt (LetStatement (LetVar varName) (Just ty) expr)) methodType types localTypes omt = do
+  exprTypes <- inferExpr expr types localTypes omt
   if Prelude.elem ty exprTypes
     then Right ((H.insert [varName] [ty] H.empty):localTypes) else Left "Types do not match" {- TODO: add line numbers -}
-checkStatement (ReturnStmt (ReturnExpr expr)) (MethodType _ _ returnType) types localTypes = do
-  exprTypes <- inferExpr expr types localTypes
+checkStatement (ReturnStmt (ReturnExpr expr)) (MethodType _ _ returnType) types localTypes omt = do
+  exprTypes <- inferExpr expr types localTypes omt
   if Prelude.elem returnType exprTypes
     then Right localTypes else Left "Return Type does not match"
-checkStatement (ReturnStmt ReturnUnit) (MethodType _ _ returnType) types localTypes =
+checkStatement (ReturnStmt ReturnUnit) (MethodType _ _ returnType) types localTypes omt =
   case returnType of
     (Primitive UnitPrimType) -> Right localTypes
     _ -> Left "Return type does not match"
-checkStatement stmt methodType types localTypes = error "TODO"
+checkStatement stmt methodType types localTypes omt = error "TODO"
 
 
 {- TODO: have a hashmap that goes from types to a hashmap of types for each class so we can typecheck members and methods -}
-inferExpr :: Expr -> (H.HashMap [String] [Type]) -> [(H.HashMap [String] [Type])] -> Either String [Type]
-inferExpr (Literal (IntLit _)) _ _ = return [(Primitive U8PrimType), (Primitive I8PrimType), (Primitive U16PrimType), (Primitive I16PrimType), (Primitive U32PrimType), (Primitive I32PrimType), (Primitive U64PrimType), (Primitive I64PrimType), (Primitive F32PrimType), (Primitive F64PrimType)] 
-inferExpr (Literal (FloatLit _)) _ _ = return [(Primitive F32PrimType), (Primitive F64PrimType)] 
-inferExpr (Literal (StringLit _)) _ _ = return [ClassType (Path ["String"] )]
-inferExpr (Literal (BoolLit _)) _ _ = return [Primitive BoolPrimType]
-inferExpr (Literal (CharLit _)) _ _ = return [Primitive CharPrimType]
-inferExpr ThisExpr _ localTypes = return $ lookupLocal localTypes ["this"]
-inferExpr SuperExpr _ localTypes = return $ lookupLocal localTypes ["super"]
-inferExpr (Paren expr) types localTypes = inferExpr expr types localTypes
-inferExpr NullExpr _ _ = return [ClassType (Path ["Object"])]
-inferExpr (Var var) _ localTypes = return $ lookupLocal localTypes [var]
-inferExpr (Cast (Primitive ty) expr) types localTypes = do
-  exprTypes <- inferExpr expr types localTypes
+inferExpr :: Expr -> TypeMap -> [TypeMap] -> ObjectMemberTypes -> Either String [Type]
+inferExpr (Literal (IntLit _)) _ _ _ = return [(Primitive U8PrimType), (Primitive I8PrimType), (Primitive U16PrimType), (Primitive I16PrimType), (Primitive U32PrimType), (Primitive I32PrimType), (Primitive U64PrimType), (Primitive I64PrimType), (Primitive F32PrimType), (Primitive F64PrimType)] 
+inferExpr (Literal (FloatLit _)) _ _ _ = return [(Primitive F32PrimType), (Primitive F64PrimType)] 
+inferExpr (Literal (StringLit _)) _ _ _ = return [ClassType (Path ["String"] )]
+inferExpr (Literal (BoolLit _)) _ _ _ = return [Primitive BoolPrimType]
+inferExpr (Literal (CharLit _)) _ _ _ = return [Primitive CharPrimType]
+inferExpr ThisExpr _ localTypes omt = return $ lookupLocal localTypes ["this"]
+inferExpr SuperExpr _ localTypes omt = return $ lookupLocal localTypes ["super"]
+inferExpr (Paren expr) types localTypes omt = inferExpr expr types localTypes omt
+inferExpr NullExpr _ _ _ = return [ClassType (Path ["Object"])]
+inferExpr (Var var) _ localTypes _ = return $ lookupLocal localTypes [var]
+inferExpr (Cast (Primitive ty) expr) types localTypes omt = do
+  exprTypes <- inferExpr expr types localTypes omt
   if exprTypes `contains` [(Primitive U8PrimType), (Primitive I8PrimType), (Primitive U16PrimType), (Primitive I16PrimType), (Primitive U32PrimType), (Primitive I32PrimType), (Primitive U64PrimType), (Primitive I64PrimType), (Primitive F32PrimType), (Primitive F64PrimType)]
     then Right [(Primitive ty)]
     else Left "Couldn't typecheck a primitive cast"
-inferExpr (Cast ty expr) types localTypes = do
+inferExpr (Cast ty expr) types localTypes omt = do
   error "casting for other types isn't implemented yet"
-inferExpr (FieldAccess ThisExpr name) types localTypes = return $ lookupLocal localTypes ["this",name]
-inferExpr (UnaryOp Neg (Literal (IntLit _))) _ _ = return [(Primitive I8PrimType), (Primitive I16PrimType), (Primitive I32PrimType), (Primitive I64PrimType), (Primitive F32PrimType), (Primitive F64PrimType)]
-inferExpr (UnaryOp Neg (Literal (FloatLit _))) _ _ = return [(Primitive F32PrimType), (Primitive F64PrimType)]
-inferExpr (UnaryOp Neg expr) types localTypes = do
-  exprTypes <- inferExpr expr types localTypes
+inferExpr (FieldAccess ThisExpr name) types localTypes _ = return $ lookupLocal localTypes ["this",name]
+inferExpr (UnaryOp Neg (Literal (IntLit _))) _ _ _ = return [(Primitive I8PrimType), (Primitive I16PrimType), (Primitive I32PrimType), (Primitive I64PrimType), (Primitive F32PrimType), (Primitive F64PrimType)]
+inferExpr (UnaryOp Neg (Literal (FloatLit _))) _ _ _ = return [(Primitive F32PrimType), (Primitive F64PrimType)]
+inferExpr (UnaryOp Neg expr) types localTypes omt = do
+  exprTypes <- inferExpr expr types localTypes omt
   if exprTypes `contains` [(Primitive U8PrimType), (Primitive I8PrimType), (Primitive U16PrimType), (Primitive I16PrimType), (Primitive U32PrimType), (Primitive I32PrimType), (Primitive U64PrimType), (Primitive I64PrimType), (Primitive F32PrimType), (Primitive F64PrimType)] {- TODO: add support for inferface impl -}
     then Right exprTypes
     else Left "Operand for - (unary) is an invalid type"
-inferExpr (BinaryOp LogicalAnd expr1 expr2) types localTypes = case ((inferExpr expr1 types localTypes), (inferExpr expr1 types localTypes)) of
+inferExpr (BinaryOp LogicalAnd expr1 expr2) types localTypes omt = case ((inferExpr expr1 types localTypes omt), (inferExpr expr1 types localTypes omt)) of
   (Left leftMsg, Left rightMsg) -> Left (leftMsg Prelude.++ "\n" Prelude.++ rightMsg)
   (Left msg, _) -> Left msg
   (_, Left msg) -> Left msg
   (Right leftList, Right rightList) -> if Prelude.elem (Primitive BoolPrimType) leftList && Prelude.elem (Primitive BoolPrimType) rightList
     then Right [Primitive BoolPrimType]
     else Left "Operands for && must be bools"
-inferExpr (BinaryOp LogicalOr expr1 expr2) types localTypes = case ((inferExpr expr1 types localTypes), (inferExpr expr1 types localTypes)) of
+inferExpr (BinaryOp LogicalOr expr1 expr2) types localTypes omt = case ((inferExpr expr1 types localTypes omt), (inferExpr expr1 types localTypes omt)) of
   (Left leftMsg, Left rightMsg) -> Left (leftMsg Prelude.++ "\n" Prelude.++ rightMsg)
   (Left msg, _) -> Left msg
   (_, Left msg) -> Left msg
